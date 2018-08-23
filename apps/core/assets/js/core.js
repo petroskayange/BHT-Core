776bd8c493d9efd5b71a8be2e30e1ec65b5cd84b
Martin Suleman <sulemartin87@gmail.com>
2018-08-23 11:18:19 +0200

    fixed streamlined config #7

--- apps/core/assets/js/core.js
+++ apps/core/assets/js/core.js
@@ -1,12 +1,14 @@
-var scheme = "http://"
-var port = ":" + "" + "/";
-var ipAddress = "localhost";
-var baseUrl = scheme + ipAddress + port;
+//declare your network configurations here
+var applicationScheme = "http://"
+var applicationPort = ":" + "3000"; //don't forget quotes  
+var applicationUrl = "localhost"; 
+var applicationBaseUrl = applicationScheme + applicationUrl + applicationPort;
+// console.log(applicationBaseUrl); uncomment this out to verify current server settings
 if (document.createElement("template").content) {
     /*Code for browsers that supports the TEMPLATE element*/
-    var namer = [];
-    var iconImage = [];
-    var description = [];
+    var applicationName = [];
+    var applicationIcon = [];
+    var applicationDescription = [];
     var xmlhttp = new XMLHttpRequest();
     var xmlhttp2 = new XMLHttpRequest();
     let appDiv = document.getElementById("modal-div");
@@ -15,19 +17,18 @@ if (document.createElement("template").content) {
             var myArr = JSON.parse(this.responseText);
             document.getElementById("modal-body");
             for (var i = 0; i < myArr.apps.length; i++) {
-                namer[i] = myArr.apps[i].name;
-                description[i] = myArr.apps[i].description;
-                iconImage[i] = myArr.apps[i].icon;
-                var keep = myArr.apps[i].folder;
-                var mainurl = baseUrl + "/apps/config/apps.json";
-                var file_url = baseUrl + "/apps/" + keep + "application.json";
-                checkJson(file_url, baseUrl, namer[i], description[i],i, iconImage[i]);
+                applicationName[i] = myArr.apps[i].applicationName;
+                applicationDescription[i] = myArr.apps[i].applicationDescription;
+                applicationIcon[i] = myArr.apps[i].applicationIcon;
+                var keep = myArr.apps[i].applicationFolder;
+                var applicationJsonUrl = applicationBaseUrl + "/apps/" + keep + "application.json";
+                checkJson(applicationJsonUrl, applicationBaseUrl, applicationName[i], applicationDescription[i],i, applicationIcon[i]);
             }
         }else if (this.status == 404) {
         	console.log("apps.json not available. Check if the file is present");
         }
     };
-    xmlhttp.open("GET", "http://0.0.0.0:0000/apps/config/apps.json", true);
+    xmlhttp.open("GET", applicationBaseUrl + "/apps/config/apps.json", true);
     xmlhttp.send();
 } else {
     /*Alternative code for browsers that do not support the TEMPLATE element*/
@@ -50,16 +51,16 @@ function newApp(applicationName, applicationDescription, applicationImage, count
     cardImage.id = "cardImage" + counter;
     cardImage.setAttribute("src", applicationImage);
     cardImage.onerror = function() { 
+    console.log(applicationImage);
     alert("Inserting alternate");
-    cardImage.src = "http://localhost:3000/public/assets/images/no_image.png"; 
+    cardImage.src = applicationBaseUrl + "/public/assets/images/no_image.png"; 
   }
 }
 
-function checkJson(file_url, baseUrl, namer, description, counter, iconUrl) {
-    fetch(file_url)
+function checkJson(applicationJsonUrl, applicationBaseUrl, applicationName, applicationDescription, counter, applicationIconUrl) {
+    fetch(applicationJsonUrl)
         .then(res => res.json())
         .then((out) => {
-            newApp(namer, description, baseUrl + iconUrl, counter);
-            // console.log(namer);
+            newApp(applicationName, applicationDescription, applicationBaseUrl + applicationIconUrl, counter);
         }).catch("no json available");
 }
