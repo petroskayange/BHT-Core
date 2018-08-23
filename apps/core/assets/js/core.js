0482ba1d40c0ae093a619f45c81c6da3a476340c
Martin Suleman <sulemartin87@gmail.com>
2018-08-23 20:19:46 +0200

    refactoring in progress

--- apps/core/assets/js/core.js
+++ apps/core/assets/js/core.js
@@ -7,29 +7,26 @@ var applicationBaseUrl = applicationScheme + applicationUrl + applicationPort;
 if (document.createElement("template").content) {
     /*Code for browsers that supports the TEMPLATE element*/
     var applicationName = [];
-    var applicationIcon = [];
     var applicationDescription = [];
-    var xmlhttp = new XMLHttpRequest();
-    var xmlhttp2 = new XMLHttpRequest();
-    let appDiv = document.getElementById("modal-div");
-    xmlhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            var myArr = JSON.parse(this.responseText);
-            document.getElementById("modal-body");
-            for (var i = 0; i < myArr.apps.length; i++) {
-                applicationName[i] = myArr.apps[i].applicationName;
-                applicationDescription[i] = myArr.apps[i].applicationDescription;
-                applicationIcon[i] = myArr.apps[i].applicationIcon;
-                var keep = myArr.apps[i].applicationFolder;
-                var applicationJsonUrl = applicationBaseUrl + "/apps/" + keep + "application.json";
-                checkJson(applicationJsonUrl, applicationBaseUrl, applicationName[i], applicationDescription[i],i, applicationIcon[i]);
-            }
-        }else if (this.status == 404) {
-        	console.log("apps.json not available. Check if the file is present");
-        }
-    };
-    xmlhttp.open("GET", applicationBaseUrl + "/apps/config/apps.json", true);
-    xmlhttp.send();
+    var applicationIcon = [];
+    $.get(applicationBaseUrl+"/apps/config/apps.json")
+    .done(function(data, status){ 
+        var applicationData = jQuery.parseJSON(JSON.stringify(data));
+        for (var i = 0; i < applicationData.apps.length; i++ ) {
+        applicationName[i] = applicationData.apps[i].applicationName;
+        applicationDescription[i] = applicationData.apps[i].applicationDescription;
+        applicationIcon[i] = applicationData.apps[i].applicationIcon;
+        console.log(i);
+        console.log(applicationIcon[i]);
+        var applicationFolder = applicationData.apps[i].applicationFolder;
+        var applicationJsonUrl = applicationBaseUrl + "/apps/" + applicationFolder + "application.json";
+        console.log(applicationJsonUrl);    
+        checkJson(applicationJsonUrl, applicationBaseUrl, applicationName[i], applicationDescription[i],i, applicationIcon[i]);
+    }
+    })
+    .fail(function(){
+    console.log("apps.json is missing from the apps/config folder");
+    }); 
 } else {
     /*Alternative code for browsers that do not support the TEMPLATE element*/
 }
@@ -38,29 +35,27 @@ function newApp(applicationName, applicationDescription, applicationImage, count
     var temp = document.getElementById("card_template");
     var clon = temp.content.cloneNode(true);
     let appDiv = document.getElementById("modal-div");
-    appDiv.appendChild(clon);
-    let applicationNameNode = document.createTextNode(applicationName);
-    let applicationDescriptionNode = document.createTextNode(applicationDescription);
     let appName = document.getElementById("appName");
-    appName.id = "appName" + counter;
-    appName.appendChild(applicationNameNode);
-    let appDescription = document.getElementById("appDescription");
-    appDescription.id = "appDescription" + counter;
-    appDescription.appendChild(applicationDescriptionNode);
     let cardImage = document.getElementById("cardImage");
+    $("#appDescription").text(applicationDescription);
+    $("#appName").text(applicationName);
+    $("#cardImage").attr('src', applicationImage);
+    appName.id = "appName" + counter;
+    appDescription.id = "appDescription" + counter;
     cardImage.id = "cardImage" + counter;
-    cardImage.setAttribute("src", applicationImage);
     cardImage.onerror = function() { 
-    console.log(applicationImage);
+    // console.log(applicationImage);
     alert("Inserting alternate");
     cardImage.src = applicationBaseUrl + "/public/assets/images/no_image.png"; 
   }
 }
 
 function checkJson(applicationJsonUrl, applicationBaseUrl, applicationName, applicationDescription, counter, applicationIconUrl) {
-    fetch(applicationJsonUrl)
-        .then(res => res.json())
-        .then((out) => {
-            newApp(applicationName, applicationDescription, applicationBaseUrl + applicationIconUrl, counter);
-        }).catch("no json available");
+    $.get(applicationJsonUrl)
+    .done(function(){ 
+    newApp(applicationName, applicationDescription, applicationBaseUrl + applicationIconUrl, counter);
+    })
+    .fail(function(){
+    console.log("The application " + applicationName + "'s application.json file is not available");
+    });
 }
