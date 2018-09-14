4639b4aef309157898ac31c8800ad60e328e071a
kajanesoko <starjsoko@gmail.com>
2018-09-14 13:02:17 +0200

    added missing files from vitals branch #98

---
+++ apps/core/assets/js/post_parameters.js
@@ -0,0 +1,38 @@
+
+
+function submitEncounter(encounter, returnToFunction) {
+  var url = apiProtocol+'://'+apiURL+':'+apiPort+'/api/v1/encounters';
+
+  var parametersPassed = JSON.stringify(encounter);
+
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 201) {
+      var obj = JSON.parse(this.responseText);
+      eval(returnToFunction)(obj['encounter_id']);
+    }
+  };
+  xhttp.open("POST", url, true);
+  xhttp.setRequestHeader('Authorization', sessionStorage.getItem("authorization"));
+  xhttp.setRequestHeader('Content-type', "application/json");
+  xhttp.send(parametersPassed);
+}
+
+
+function createObs(observations, returnToFunction){
+  var url = apiProtocol+'://'+apiURL+':'+apiPort+'/api/v1/observations';
+
+  var parametersPassed = JSON.stringify(observations);
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 201) {
+      var objs = JSON.parse(this.responseText);
+      eval(returnToFunction)(objs);
+    }
+  };
+  xhttp.open("POST", url, true);
+  xhttp.setRequestHeader('Authorization', sessionStorage.getItem("authorization"));
+  xhttp.setRequestHeader('Content-type', "application/json");
+  xhttp.send(parametersPassed);
+
+}
