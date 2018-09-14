5328ab54f0b6ec97eea32afcfed0f8c25661306e
Martin Suleman <sulemartin87@gmail.com>
2018-09-14 19:08:00 +0200

    added generic ajax function

--- apps/core/assets/js/generic_ajaxrequest.js
+++ apps/core/assets/js/generic_ajaxrequest.js
@@ -1,18 +1,34 @@
-var patientID = patient_id
+// var patientID = patient_id
+var url = new URL(url);
+var apiURL = sessionStorage.getItem("apiURL");
+var apiPort = sessionStorage.getItem("apiPort");
+var apiProtocol = sessionStorage.getItem("apiProtocol");
+var id = url.searchParams.get("user_id");
+var applicationName = sessionStorage.getItem("applicationName");
 
-function checkIfEncounterCaptured(encounterType, name){
-    jQuery.ajax({
-      type: "POST",
-      url: "/patients/encounters",
-      data: "",
-      success: function(result){
-      	if(result == 'true'){
-      		jQuery("#next-task").html(result);	
-      	}
-        
+function checkIfEncounterCaptured(encounterType, name) {
+  
+    var url = '/apps/' + applicationName[0] +'/application.json';;
+    // console.log(applicationName);
+    var req = new XMLHttpRequest();
+    req.onreadystatechange = function () {
+      // console.log(encounterType);
+      if (this.readyState == 4 && this.status == 200) {
+        var results = JSON.parse(this.responseText);
+        var available = results.encounters[encounterType].available;
+        var url = results.encounters[encounterType].url;
+        console.log(available);
+        if (available == false) {
+          window.location.href = '/';
+        }else {
+          window.location.href = url;
+        }
       }
-    });
-  }
-
-
-
+    };
+    try {
+      req.open('GET', url, true);
+      req.setRequestHeader('Authorization', sessionStorage.getItem('authorization'));
+      req.send(null);
+    } catch (e) {}
+  
+}
