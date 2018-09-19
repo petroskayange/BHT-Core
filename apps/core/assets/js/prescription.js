38983b3065f9f201a12835665a2df0e7089e10ab
mwatha <mwathabwanali@gmail.com>
2018-09-19 13:19:18 +0200

    adding pescription page: 70%

--- apps/core/assets/js/prescription.js
+++ apps/core/assets/js/prescription.js
@@ -1,6 +1,7 @@
 
 function buildPrescriptionPage() {
   var iframe = document.getElementById("inputFrame" + tstCurrentPage);
+  iframe.style = "width: 96%;";
 
   var main_table = document.createElement("div");
   main_table.setAttribute("class","main-table");
@@ -60,7 +61,7 @@ function buildMedicationSearch() {
   results_divs_row.appendChild(main_table_cell);
 
   addKeyboard()
-  displaySelected();
+  displaySelected("results-divs-table-cell-right");
 }
 
 function addSelectionTable() {
@@ -131,6 +132,8 @@ function renderResults(medication) {
     li.innerHTML = medication[i].name;
     li.setAttribute("tstvalue", medication[i].name);
     li.setAttribute("class", "results-list");
+    li.setAttribute("dose_strength", medication[i].dose_strength);
+    li.setAttribute("units", medication[i].units);
     li.setAttribute("id", medication[i].drug_id);
     li.setAttribute("onmousedown", "null; updateSelection(this);");
     ul.appendChild(li);
@@ -149,7 +152,12 @@ function updateSelection(li) {
 
   li.style = "background-color: lightblue;";
 
-  selected_meds[li.id] = li.innerHTML;
+  selected_meds[li.id] = {
+    name: li.innerHTML, am: null, 
+    noon: null, pm: null,
+    dose_strength: li.getAttribute("dose_strength"),
+    units: li.getAttribute("units")
+  };
   displaySelected("results-divs-table-cell-right");  
 }
 
@@ -160,6 +168,9 @@ function displaySelected(container_name) {
 
   for(var drug_id in selected_meds){
     var row = document.createElement("tr");
+    row.setAttribute("id","row-" + drug_id);
+    row.setAttribute("class","selected-meds-row");
+    row.setAttribute("onmousedown","setDosage(this, '" + container_name + "');");
     table.appendChild(row);
 
     var td = document.createElement("td");
@@ -167,19 +178,53 @@ function displaySelected(container_name) {
     
     var img = document.createElement("img");
     img.setAttribute("src","../../assets/images/delete.png");
-    img.setAttribute("draggable", "false");
+    img.setAttribute("id", drug_id);
+    img.setAttribute("onmousedown", "deleteFromList(this,'" + container_name + "');");
     img.setAttribute("class","icons");
     td.appendChild(img);
     row.appendChild(td);
 
     var td = document.createElement("td");
-    td.innerHTML = selected_meds[drug_id];
+    td.innerHTML = selected_meds[drug_id].name;
     row.appendChild(td);
 
 
   }
 
   mainContainer.appendChild(table);
+}
+
+function setDosage(e, container_name) {
+  if(container_name != "dosage-table-cell-left")
+    return;
+
+  var rows = document.getElementsByClassName("selected-meds-row");
+  for(var i = 0 ; i < rows.length ; i++){
+    rows[i].style = "background-color: none; color: black;";
+  }
+  e.style = "color: black; background-color: #87ceeb;";
+  resetDosageContainers();
+  
+  var drug_id = e.id.replace("row-","");
+  document.getElementById("text-am").value    = selected_meds[drug_id].am;
+  document.getElementById("text-noon").value  = selected_meds[drug_id].noon;
+  document.getElementById("text-pm").value    = selected_meds[drug_id].pm;
+   
+}
+
+function resetDosageContainers() {
+  document.getElementById("text-am").value = null;
+  document.getElementById("text-noon").value = null;
+  document.getElementById("text-pm").value = null;
+}
+
+function deleteFromList(e, container_name) {
+  delete selected_meds[e.id];
+  displaySelected(container_name);
+
+  if(container_name.match(/dosage-table-cell-left/i)){
+    resetDosageContainers();
+  }
 }
 
 function getMedication(search_str) {
@@ -264,6 +309,7 @@ function buildPage(e) {
   }else if(e.innerHTML.match(/Duration/i)) {
     buildDuration();
   }else if(e.innerHTML.match(/Summary/i)) {
+    buildSummary();
   }
 }
 
@@ -328,6 +374,7 @@ function buildDosageKeyPad() {
     var inputBox = document.createElement("input");
     inputBox.setAttribute("type","text");
     inputBox.setAttribute("class","input-boxes");
+    inputBox.setAttribute("onfocus","setKeyPadFocus(this)")
     inputBox.setAttribute("id","text-" + tds[i][0].toLowerCase());
     td.appendChild(inputBox);
 
@@ -348,6 +395,58 @@ function buildDosageKeyPad() {
   buildKeyPad(td);
 }
 
+function setKeyPadFocus(e) {
+  var keys = document.getElementsByClassName("keypad-buttons");
+  
+  for(var i = 0 ; i < keys.length ; i++){
+    keys[i].setAttribute("onmousedown", "dosageEntered(this,'" + e.id + "');");
+  }
+
+}
+
+function getRowSelected(){
+  var rows = document.getElementsByClassName("selected-meds-row");
+  var row = null;
+  for(var i = 0 ; i < rows.length ; i++){
+    var styles = rows[i].style.backgroundColor;
+    if(styles == "rgb(135, 206, 235)")
+      row = rows[i];
+
+  }
+  return row;
+}
+
+function dosageEntered(key, text_box_id){
+  var inputBox = document.getElementById(text_box_id);
+  rowSelected = getRowSelected();
+
+  if(!rowSelected)
+    return;
+
+  var drug_id = rowSelected.id.replace("row-","");
+
+  try{
+
+    if(key.innerHTML.match(/Del/i)){
+      inputBox.value = inputBox.value.substring(0, inputBox.value.length - 1);
+    }else if(key.innerHTML.match(/Clear/i)){
+      inputBox.value = null;
+    }else{
+      inputBox.value += key.innerHTML;
+    }
+
+    if(text_box_id.match(/am/i)){
+      selected_meds[drug_id].am = inputBox.value;
+    }else if(text_box_id.match(/noon/i)){
+      selected_meds[drug_id].noon = inputBox.value;
+    }else if(text_box_id.match(/pm/i)){
+      selected_meds[drug_id].pm = inputBox.value;
+    }
+  }catch(x) { }
+
+}
+
+
 function buildKeyPad(e){
   var table = document.createElement('table');
   table.setAttribute("class","prescription-keypad");
@@ -360,7 +459,7 @@ function buildKeyPad(e){
   keypad_attributes.push([4,5,6]);
   keypad_attributes.push([7,8,9]);
   keypad_attributes.push(["Del.",0,"."]);
-  keypad_attributes.push(["Clear","%","/"]);
+  //keypad_attributes.push(["Clear","%","/"]);
 
   for(var i = 0 ; i < keypad_attributes.length ; i++) {
     var tr = document.createElement("tr");
@@ -383,5 +482,129 @@ function buildKeyPad(e){
 }
 
 function buildDuration() {
+  var mainContainer = document.getElementById("main-table-cell-right");
+
+  var table = document.createElement("table");
+  table.setAttribute("id", "duration-table");
+
+  var tr = document.createElement("tr");
+  table.appendChild(tr)
+  var td = document.createElement("td");
+  tr.appendChild(td)
+
+  var label = document.createElement("label");
+  label.innerHTML = "Duration (days)"
+  label.setAttribute("id","duration-label");
+  td.appendChild(label);
+
+
+  var tr = document.createElement("tr");
+  table.appendChild(tr)
+  var td = document.createElement("td");
+  tr.appendChild(td)
+
+  var inputBox = document.createElement("input");
+  inputBox.setAttribute("type","text");
+  inputBox.setAttribute("id","duration-text");
+  inputBox.value  = setDuration;
+  td.appendChild(inputBox);
+  
+
+  var tr = document.createElement("tr");
+  table.appendChild(tr)
+  var td = document.createElement("td");
+  tr.appendChild(td)
+
+  buildKeyPad(td);
+
+
+  mainContainer.appendChild(table);
+
+  var keys = document.getElementsByClassName("keypad-buttons");
+  for(var i = 0 ; i < keys.length ; i++){
+    keys[i].setAttribute("onmousedown","enterDuration(this);");
+  }
+
 }
 
+var setDuration = null;
+
+function enterDuration(key) {
+  var inputBox = document.getElementById("duration-text");
+
+  try{
+
+    if(key.innerHTML.match(/Del/i)){
+      inputBox.value = inputBox.value.substring(0, inputBox.value.length - 1);
+    }else if(key.innerHTML.match(/Clear/i)){
+      inputBox.value = null;
+    }else{
+      inputBox.value += key.innerHTML;
+    }
+    setDuration = inputBox.value;
+  }catch(x) { }
+
+}
+
+
+function buildSummary() {
+  var table = document.createElement("table");
+  table.setAttribute("id","summary-table");
+
+  var tr  = document.createElement("tr");
+  table.appendChild(tr);
+  var headers = ["Medication","Duration","Instruction","Total Quantity"];
+
+  for(var i = 0 ; i < headers.length ; i++){
+    var th = document.createElement("th");
+    th.innerHTML = headers[i];
+    th.setAttribute("class","summary-headers");
+    if(i == 0 || i == 2)
+      th.setAttribute("style","width: 30%;");
+
+    tr.appendChild(th);
+  }
+
+
+
+  for(drug_id in selected_meds){
+    var tr  = document.createElement("tr");
+    var td = document.createElement("td");
+    td.innerHTML = selected_meds[drug_id].name;
+    tr.appendChild(td);
+/*
+    td = document.createElement("td");
+    td.innerHTML = selected_meds[drug_id].dose_strength;
+    tr.appendChild(td);
+
+    td = document.createElement("td");
+    td.innerHTML = selected_meds[drug_id].dose_strength;
+    tr.appendChild(td);
+*/
+    td = document.createElement("td");
+    td.innerHTML = setDuration;
+    tr.appendChild(td);
+
+    td = document.createElement("td");
+    td.innerHTML = "<b>AM</b>: " + selected_meds[drug_id].am;
+    td.innerHTML += " <b>Noon</b>: " + selected_meds[drug_id].noon;
+    td.innerHTML += " <b>PM</b>: " + selected_meds[drug_id].pm;
+    tr.appendChild(td);
+
+    td = document.createElement("td");
+    td.innerHTML = getTotalPillsNeed(drug_id) + " " + selected_meds[drug_id].units;
+    tr.appendChild(td);
+    table.appendChild(tr);
+  }
+
+  var mainContainer = document.getElementById("main-table-cell-right");
+  mainContainer.appendChild(table);
+}
+
+function getTotalPillsNeed(drug_id) {
+  var am    = parseFloat(selected_meds[drug_id].am);
+  var noon  = parseFloat(selected_meds[drug_id].noon);
+  var pm    = parseFloat(selected_meds[drug_id].pm);
+
+  return (parseInt(setDuration)* (am + noon + pm));
+}
