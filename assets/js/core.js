b26f0d43f51318ca87b46a3c904db82d27a8f35d
mwatha <mwathabwanali@gmail.com>
2018-10-04 16:00:10 +0200

    merging

--- assets/js/core.js
+++ assets/js/core.js
@@ -143,12 +143,12 @@ function generateActivities() {
     if (document.createElement("template").content) {
         /*Code for browsers that supports the TEMPLATE element*/
      
-        $.getJSON("/apps/BHT-Core-Apps-ART/application.json")
+        $.getJSON("/apps/ART/application.json")
             .done(function (data, status) {
                 getActivities(data);
             })
             .fail(function () {
-                console.log("application.json is missing from the /apps/BHT-Core-Apps-ART folder");
+                console.log("application.json is missing from the /apps/ART folder");
             });
     } else {
         /*Alternative code for browsers that do not support the TEMPLATE element*/
@@ -304,12 +304,12 @@ function parser(applicationData) {
 
 function getActivities(activitiesData) {
 
-    for (var i = 0; i < activitiesData.activities.length; i++) {
+    for (var i = 0; i < activitiesData.others.length; i++) {
 
-        activitiesName[i] = activitiesData.activities[i].activitiesName || "Activities Name Not Defined!!";
-        activitiesDescription[i] = activitiesData.activities[i].activitiesDescription || "No Description Available";
-        activitiesIcon[i] = activitiesData.activities[i].activitiesIcon;
-        applicationFolder[i] = activitiesData.activities[i].applicationFolder;
+        activitiesName[i] = activitiesData.others[i].activitiesName || "Activities Name Not Defined!!";
+        activitiesDescription[i] = activitiesData.others[i].activitiesDescription || "No Description Available";
+        activitiesIcon[i] = activitiesData.others[i].activitiesIcon;
+        applicationFolder[i] = activitiesData.others[i].applicationFolder;
         checkActivities(applicationJsonUrl[i], activitiesName[i], activitiesDescription[i], i, activitiesIcon[i]);
 
     }
