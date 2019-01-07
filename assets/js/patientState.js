1c3f1517b4c472f6db6d874c4eb7d4f0208df511
danielmwakanema <danielmwakanema95@gmail.com>
2019-01-07 11:05:01 +0200

    refactor(createPatientState): Transpile down JS to ES2015
    - Transpiled down the JavaScript to ES2015 in order for it to work on the J2 terminals

--- assets/js/patientState.js
+++ assets/js/patientState.js
@@ -1,74 +1,68 @@
-function getProgramWorkflowStates (options = {}) {
-  const apiPath = `${apiProtocol}://${apiURL}:${apiPort}/api/v1/programs/${options.programId}/workflows`
-  GET (
-    {
-      url: apiPath,
-      async: true,
-      headers: {
-        'Authorization': options.authToken
-      }
-    },
-    {},
-    (data) => {
-      options.success(data)
-    },
-    (error) => {
-      options.error(error)
+function getProgramWorkflowStates() {
+  var options = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
+
+  var apiPath = apiProtocol + '://' + apiURL + ':' + apiPort + '/api/v1/programs/' + options.programId + '/workflows';
+  GET({
+    url: apiPath,
+    async: true,
+    headers: {
+      'Authorization': options.authToken
     }
-  )
+  }, {}, function (data) {
+    options.success(data);
+  }, function (error) {
+    options.error(error);
+  });
 }
 
-function getStateIdByName (options = {}) {
-  getProgramWorkflowStates (
-    {
-      programId: sessionStorage.getItem('programID'),
-      authToken: sessionStorage.getItem('authorization'),
-      success: (data) => {
-        const stateId = data[0].states.filter((state) => {
-          return state.concept.concept_names.every((concept_name) => {
-            return concept_name.name === options.stateName
-          })
-        })[0].program_workflow_state_id
-        if (!stateId) {
-          options.error(new Error(`getStateIdByName: The passed state with name (${options.stateName}), does not exist.`))
-        } else {
-          options.success(stateId)
+function getStateIdByName() {
+  var options = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
+
+  getProgramWorkflowStates({
+    programId: sessionStorage.getItem('programID'),
+    authToken: sessionStorage.getItem('authorization'),
+    success: function success(data) {
+      var stateId = data[0].states.filter(function (state) {
+        return state.concept.concept_names.every(function (concept_name) {
+          return concept_name.name === options.stateName;
+        });
+      })[0].program_workflow_state_id;
+      if (!stateId) {
+        options.error(new Error('getStateIdByName: The passed state with name (' + options.stateName + '), does not exist.'));
+      } else {
+        options.success(stateId);
+      }
+    },
+    error: function error(_error) {
+      options.error(_error);
+    }
+  });
+}
+
+function createPatientState() {
+  var options = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
+
+  var apiPath = apiProtocol + '://' + apiURL + ':' + apiPort + '/api/v1/programs/' + options.programId + '/patients/' + options.patientId + '/states';
+  getStateIdByName({
+    stateName: options.stateName,
+    success: function success(stateId) {
+      POST({
+        url: apiPath,
+        async: true,
+        headers: {
+          'Authorization': options.authToken,
+          'Content-Type': 'application/x-www-form-urlencoded'
         }
-      },
-      error: (error) => {
-        options.error(error)
-      }
+      }, {
+        state: stateId
+      }, function (data) {
+        options.success(data);
+      }, function (error) {
+        options.error(error);
+      });
+    },
+    error: function error(_error2) {
+      options.error(_error2);
     }
-  )
-}
-
-function createPatientState (options = {}) {
-  const apiPath = `${apiProtocol}://${apiURL}:${apiPort}/api/v1/programs/${options.programId}/patients/${options.patientId}/states`
-  getStateIdByName(
-    {
-      stateName: options.stateName,
-      success: (stateId) => {
-        POST (
-          {
-            url: apiPath,
-            async: true,
-            headers: {
-              'Authorization': options.authToken,
-              'Content-Type': 'application/x-www-form-urlencoded'
-            }
-          },
-          { state: stateId },
-          (data) => {
-            options.success(data)
-          },
-          (error) => {
-            options.error(error)
-          }
-        )
-      },
-      error: (error) => {
-        options.error(error)
-      }
-    }
-  )
+  });
 }
