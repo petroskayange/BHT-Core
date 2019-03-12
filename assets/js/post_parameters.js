dd44a03cd818739fb16a3796f5539d9e7416eeff
Martin Suleman <sulemartin87@gmail.com>
2019-03-12 09:43:52 +0200

    added remote and local duplicate check

--- assets/js/post_parameters.js
+++ assets/js/post_parameters.js
@@ -14,15 +14,32 @@ function submitParameters(parameters, url, returnToFunction) {
   
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && (this.status == 201 || this.status == 200)) {
-      var obj = JSON.parse(this.responseText);
-      eval(returnToFunction)(obj);
-	 try {    
-      		document.getElementById("innerPop").style.display = "none";
-	 }catch(e) {
-
-	 }
-    }
+    if (this.readyState == 4 ) {
+      if ( (this.status == 201 || this.status == 200)) {
+        var obj = JSON.parse(this.responseText);
+          eval(returnToFunction)(obj);
+          try {    
+                  document.getElementById("innerPop").style.display = "none";
+          }catch(e) {
+        
+          }
+      }else if (this.status == 404 || this.status == 400 || this.status == 500) {
+        alertify.alert('An error has occured', 'Error' + this.status , function(){ alertify.success('Ok'); });
+  
+      }
+      }else {
+       
+        alertify.alert('An error has occured', 'Error' + this.status , function(){
+           var submitCover = document.getElementById('submit-cover');
+           document.getElementsByClassName("ajs-button ajs-ok")[0].style.background = "";
+           document.getElementById("innerPop").style.display = "none";
+           document.getElementById("popupBox").style.display = "none";
+           if (submitCover){
+               submitCover.parentNode.removeChild(submitCover);
+           }
+          });
+      }
+      console.log(this.status);
   };
   xhttp.open("POST", url, true);
   xhttp.setRequestHeader('Authorization', sessionStorage.getItem("authorization"));
