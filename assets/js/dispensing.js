19db5745f6e8f61e8bbcd6c4059ba533245d5ede
Mwatha Bwanali <mwathabwanali@gmail.com>
2019-05-29 12:45:57 +0200

    fixing amount need bug

--- assets/js/dispensing.js
+++ assets/js/dispensing.js
@@ -417,7 +417,7 @@ function addPrescriptions(data) {
         var amount_needed = data[i].amount_needed;
         var quantity = data[i].quantity;
 
-        var complete_pack = calculate_complete_pack(data[i], amount_needed) - (quantity || 0)
+        var complete_pack = calculate_complete_pack(data[i], parseFloat(amount_needed)) - (quantity || 0)
         fetchedPrescriptions[drug_id] = order_id;
         complete_pack = complete_pack < 0 ? 0 : complete_pack;
 
@@ -718,7 +718,7 @@ function calculate_complete_pack(drug, units) {
     var drug_order_barcodes = drug.barcodes.sort(function (a, b) {
         return a.tabs - b.tabs;
     }); //sorting in an ascending order by tabs
-    if (drug_order_barcodes.length == 0 || parseFloat(units) == 0.0) {
+    if (drug_order_barcodes.length == 0 || units == 0.0) {
         return units;
     }
 
@@ -731,7 +731,7 @@ function calculate_complete_pack(drug, units) {
     var smallest_available_tab = parseInt(drug_order_barcodes[0].tabs)
     var complete_pack = parseInt(drug_order_barcodes[drug_order_barcodes.length - 1].tabs)
 
-    while (complete_pack < units.to_f) {
+    while (complete_pack < units) {
         complete_pack += smallest_available_tab
     }
 
