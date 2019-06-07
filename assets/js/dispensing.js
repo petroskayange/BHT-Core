6e3710d23c120af608f77529119d7c339080766a
Martin Suleman <sulemartin87@gmail.com>
2019-06-07 09:51:42 +0200

    fixed missing page protocols

--- assets/js/dispensing.js
+++ assets/js/dispensing.js
@@ -501,14 +501,11 @@ function addDispBTN(order_id) {
 }
 
 function addReset(order_id) {
-    //var row = document.getElementById(order_id);
-    //row.setAttribute("onmousedown", "displayKeyPad('" + order_id + "');");
     var span = document.createElement("span");
     var btn = document.createElement("img");
     btn.setAttribute("src","/assets/images/reset.png");
-    btn.style.maxWidth = "50%";
+    btn.style.width = "20%";
     btn.setAttribute("onmousedown", "voidDrugDispensations('" + order_id + "');");
-    btn.innerHTML = "Dispense";
     span.appendChild(btn);
     return span.innerHTML;
 }
