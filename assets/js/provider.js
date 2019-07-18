77b54fb5640a442cb0c5124531afa62dd1072deb
kajanesoko <starjsoko@gmail.com>
2019-07-18 15:53:31 +0200

    added extra conditions for saving provider in vmmc

--- assets/js/provider.js
+++ assets/js/provider.js
@@ -1,4 +1,6 @@
 var providerID = null;
+var providerName = null;
+var programID = sessionStorage.getItem("programID");
 function showProviderModal() {
 
     var providerModal = document.createElement('div');
@@ -32,6 +34,13 @@ function showProviderModal() {
     
 }
 
+function changeModalHeader() {
+    var field = $("touchscreenInput" + tstCurrentPage);
+        var modalHeader = document.getElementsByClassName('provider-modal-header')[0];
+        modalHeader.innerHTML = '<h2 style="margin-top: 0; color: white; padding: 10px;">Select assistant circumciser </h2>';
+        modalHeader.style.color= "white"
+    }
+
 function getProviders(modalBody){
     var url = 'http://'+apiURL+':'+apiPort+'/api/v1/users?role=Superuser' ;
     var req = new XMLHttpRequest();
@@ -52,7 +61,7 @@ function getProviders(modalBody){
             }catch(e) {
 
             }
-            li.setAttribute('onclick', 'selectReason(this)');
+            li.setAttribute('onclick', 'selectProvider(this)');
             ul.appendChild(li);
         }
 
@@ -68,7 +77,7 @@ function getProviders(modalBody){
 }
 }
 
-function selectReason(element) {
+function selectProvider(element) {
         var items = document.getElementsByClassName("providers active");
         for (let index = 0; index < items.length; index++) {
             items[index].className = "providers";
@@ -81,6 +90,7 @@ function setProvider() {
     var providerElemnt = document.getElementsByClassName("providers active");
     if(providerElemnt.length > 0) {
         providerID =  providerElemnt[0].getAttribute("provider-id");
+        providerName =  providerElemnt[0].innerHTML;
         document.getElementById("provider-modal").style.display = "none";
         var elem = document.getElementsByClassName("provider-list")[0];
         elem.remove();
